004245a568fb0db20fa1c3fc4fe6bb3df3142815
Anne13006 <claverie.anne13006@gmail.com>
2021-04-27 15:43:04 +0200

    Quetes 14 add Actor table and form

---
+++ src/Entity/Actor.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ActorRepository::class)
+ */
+class Actor
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Program::class, inversedBy="Actor")
+     */
+    private $program;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $birthdate;
+
+    public function __construct()
+    {
+        $this->program = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Program[]
+     */
+    public function getProgram(): Collection
+    {
+        return $this->program;
+    }
+
+    public function addProgram(Program $program): self
+    {
+        if (!$this->program->contains($program)) {
+            $this->program[] = $program;
+        }
+
+        return $this;
+    }
+
+    public function removeProgram(Program $program): self
+    {
+        $this->program->removeElement($program);
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(?\DateTimeInterface $birthdate): self
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+}
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -18,166 +18,199 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  */
 
  class Program
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+               {
+                   /**
+                    * @ORM\Id
+                    * @ORM\GeneratedValue
+                    * @ORM\Column(type="integer")
+                    */
+                   private $id;
+               
+                   /**
+                    * @ORM\Column(type="string", length=255, unique=true)
+                    * @Assert\NotBlank(message="Program title should not be EMPTY")
+                    * @Assert\Length(max="25", maxMessage="Nom saisie {{ value }} est trop longue, ne doit pas dépasser {{ limit }}caractères")
+                    */
+                   private $title;
+               
+                   /**
+                    * @ORM\Column(type="text")
+                    * @Assert\NotBlank(message="Le résumé du Programme ne doit pas etre vide svp")
+                    */
+                   private $summary;
+               
+                   /**
+                    * @ORM\Column(type="string", length=255, nullable=true)
+                    */
+                   private $poster;
+               
+                   /**
+                    * @ORM\OneToMany(targetEntity=Season::class, mappedBy="program")
+                    */
+                   private $seasons;
+               
+                   /**
+                    * @ORM\Column(type="string", length=255)
+                    */
+                   private $country;
+               
+                   /**
+                    * @ORM\Column(type="integer")
+                    */
+                   private $year;
+               
+                   /**
+                    * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
+                    */
+                   private $category;
+            
+                   /**
+                    * @ORM\ManyToMany(targetEntity=Actor::class, mappedBy="program")
+                    */
+                   private $actor;
+               
+               
+                   public function __construct()
+                   {
+                       $this->seasons = new ArrayCollection();
+                       $this->programs = new ArrayCollection();
+                       $this->actor = new ArrayCollection();
+                   }
+               
+                   
+               
+                   public function getId(): ?int
+                   {
+                       return $this->id;
+                   }
+               
+                   public function getTitle(): ?string
+                   {
+                       return $this->title;
+                   }
+               
+                   public function setTitle(string $title): self
+                   {
+                       $this->title = $title;
+               
+                       return $this;
+                   }
+               
+                   public function getSummary(): ?string
+                   {
+                       return $this->summary;
+                   }
+               
+                   public function setSummary(string $summary): self
+                   {
+                       $this->summary = $summary;
+               
+                       return $this;
+                   }
+               
+                   public function getPoster(): ?string
+                   {
+                       return $this->poster;
+                   }
+               
+                   public function setPoster(?string $poster): self
+                   {
+                       $this->poster = $poster;
+               
+                       return $this;
+                   }
+               
+                   /**
+                    * @return Collection|Season[]
+                    */
+                   public function getSeasons(): Collection
+                   {
+                       return $this->seasons;
+                   }
+               
+                   public function addSeason(Season $season): self
+                   {
+                       if (!$this->seasons->contains($season)) {
+                           $this->seasons[] = $season;
+                           $season->setProgram($this);
+                       }
+               
+                       return $this;
+                   }
+               
+                   public function removeSeason(Season $season): self
+                   {
+                       if ($this->seasons->removeElement($season)) {
+                           // set the owning side to null (unless already changed)
+                           if ($season->getProgram() === $this) {
+                               $season->setProgram(null);
+                           }
+                       }
+               
+                       return $this;
+                   }
+               
+                   public function getCountry(): ?string
+                   {
+                       return $this->country;
+                   }
+               
+                   public function setCountry(string $country): self
+                   {
+                       $this->country = $country;
+               
+                       return $this;
+                   }
+               
+                   public function getYear(): ?int
+                   {
+                       return $this->year;
+                   }
+               
+                   public function setYear(int $year): self
+                   {
+                       $this->year = $year;
+               
+                       return $this;
+                   }
+               
+                   public function getCategory(): ?Category
+                   {
+                       return $this->category;
+                   }
+               
+                   public function setCategory(?Category $category): self
+                   {
+                       $this->category = $category;
+               
+                       return $this;
+                   }
+      
+                   /**
+                    * @return Collection|Actor[]
+                    */
+                   public function getActor(): Collection
+                   {
+                       return $this->actor;
+                   }
+   
+                   public function addActor(Actor $actor): self
+                   {
+                       if (!$this->actor->contains($actor)) {
+                           $this->actor[] = $actor;
+                           $actor->addProgram($this);
+                       }
+   
+                       return $this;
+                   }
 
-    /**
-     * @ORM\Column(type="string", length=255, unique=true)
-     * @Assert\NotBlank(message="Program title should not be EMPTY")
-     * @Assert\Length(max="25", maxMessage="Nom saisie {{ value }} est trop longue, ne doit pas dépasser {{ limit }}caractères")
-     */
-    private $title;
+                   public function removeActor(Actor $actor): self
+                   {
+                       if ($this->actor->removeElement($actor)) {
+                           $actor->removeProgram($this);
+                       }
 
-    /**
-     * @ORM\Column(type="text")
-     * @Assert\NotBlank(message="Le résumé du Programme ne doit pas etre vide svp")
-     */
-    private $summary;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $poster;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Season::class, mappedBy="program")
-     */
-    private $seasons;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $country;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $year;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
-     */
-    private $category;
-
-
-    public function __construct()
-    {
-        $this->seasons = new ArrayCollection();
-        $this->programs = new ArrayCollection();
-    }
-
-    
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getSummary(): ?string
-    {
-        return $this->summary;
-    }
-
-    public function setSummary(string $summary): self
-    {
-        $this->summary = $summary;
-
-        return $this;
-    }
-
-    public function getPoster(): ?string
-    {
-        return $this->poster;
-    }
-
-    public function setPoster(?string $poster): self
-    {
-        $this->poster = $poster;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Season[]
-     */
-    public function getSeasons(): Collection
-    {
-        return $this->seasons;
-    }
-
-    public function addSeason(Season $season): self
-    {
-        if (!$this->seasons->contains($season)) {
-            $this->seasons[] = $season;
-            $season->setProgram($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSeason(Season $season): self
-    {
-        if ($this->seasons->removeElement($season)) {
-            // set the owning side to null (unless already changed)
-            if ($season->getProgram() === $this) {
-                $season->setProgram(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getYear(): ?int
-    {
-        return $this->year;
-    }
-
-    public function setYear(int $year): self
-    {
-        $this->year = $year;
-
-        return $this;
-    }
-
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?Category $category): self
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
-}
+                       return $this;
+                   }
+               
+               }
